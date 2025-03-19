3a8fe1ad50f499aca777dbf3ad7458b4ba248d99
Mohamed Abdelhakeem <mohamed.abdelhakeemx@gmail.com>
2025-03-19 13:09:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = '123 456 789'
 
 db = SQLAlchemy(app)
